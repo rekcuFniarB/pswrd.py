adcdfe1e8a89da00b7da5ced768c4c4b144fe0d5
Pyroman <retratserif@gmail.com>
2020-07-18 23:21:40 +0000

    Layouting

--- src/main.py
+++ src/main.py
@@ -70,13 +70,21 @@ class LoginScreen(GridLayout, Screen):
     def __init__(self, **kwargs):
         super(LoginScreen, self).__init__(**kwargs)
         self.cols = 1
+        self.row_force_default = True
+        self.row_default_height = 60
+        #self.col_force_default = True
+        #self.col_default_width = 400
         #self.add_widget(Label(text='User Name'))
         #self.username = TextInput(multiline=False)
         #self.add_widget(self.username)
-        self.add_widget(Label(text='Password'))
-        self.password = TextInput(password=True, multiline=False, size_hint_y=None, height=32, width=100)
+        #self.center = (0.5, 0.5)
+        self.size_hint_x = None
+        #self.width = 300
+        self.pos_hint = {'center_x': 0.21}
+        self.add_widget(Label(text='Master password:', size_hint=(1, None), height=60, center=(0.5, 0.5)))
+        self.password = TextInput(password=True, multiline=False, size_hint=(None, None), width=300, height=32, center=(0.5, 0.5))
         self.add_widget(self.password)
-        self.open = Button(text='Open')
+        self.open = Button(text='Open', size_hint=(None, None), width=300, height=32, center=(0.5, 0.5))
         self.open.bind(on_press=self.btn_open)
         self.add_widget(self.open)
     
@@ -84,19 +92,26 @@ class LoginScreen(GridLayout, Screen):
         ''' "Open" button click handler. '''
         main.password = main.loginScreen.password.text
         main.root.current = 'main'
-        print(main.password)
+        #print(main.password)
 
 class MainScreen(GridLayout, Screen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.cols = 1
+        #self.center = (0.5, 0.5)
+        self.size_hint_x = None
+        #self.width = 300
+        self.pos_hint = {'center_x': 0.18}
+        self.row_force_default = True
+        self.row_default_height = 50
         self.add_widget(Menu())
-        self.add_widget(Label(text='User Name (optional)'))
-        self.userName = TextInput(multiline=False, height=32)
+        self.add_widget(Label(text='User Name (optional):', size_hint_y=None, height=30, center=(0, 0)))
+        self.userName = TextInput(multiline=False, size_hint=(None,None), size=(300, 32),
+            pos_hint={'center_x': 100}, center=(0, 0))
         self.add_widget(self.userName)
         
         ## 2 columns row
-        self.type = GridLayout(cols=2, height=32)
+        self.type = GridLayout(cols=2, height=32, width=300, size_hint=(None, None), center=(0, 0), pos_hint = {'center_x': 0.21})
         self.type.add_widget(Label(text='Type:', height=32))
         self.type.drop = DropDown()
         self.type.types = ('None', 'Web', 'Email', 'Chat', 'Other')
@@ -110,32 +125,37 @@ class MainScreen(GridLayout, Screen):
         self.type.add_widget(self.type.btn_drop)
         self.add_widget(self.type)
         
-        self.add_widget(Label(text='Domain'))
-        self.domain = TextInput(multiline=False, height=32)
+        self.add_widget(Label(text='Domain:'))
+        self.domain = TextInput(multiline=False, size_hint_y=None, height=32)
         self.add_widget(self.domain)
         
         ## 2 columns row
-        self.version = GridLayout(cols=2)
-        self.version.add_widget(Label(text='Alnum:'))
+        self.version = obj2 = type('', (), {})
+        _ = GridLayout(cols=2)
+        _.add_widget(Label(text='Alnum:'))
         self.version.alnum = CheckBox()
-        self.version.add_widget(self.version.alnum)
-        self.version.add_widget(Label(text='Compat:'))
+        _.add_widget(self.version.alnum)
+        self.add_widget(_)
+        _ = GridLayout(cols=2)
+        _.add_widget(Label(text='Compat:'))
         self.version.compat = CheckBox()
-        self.version.add_widget(self.version.compat)
-        self.version.add_widget(Label(text='Version (default: 1)'))
-        self.version.version = TextInput(multiline=False, text='1', width=32, height=32)
-        self.version.add_widget(self.version.version)
-        self.add_widget(self.version)
+        _.add_widget(self.version.compat)
+        self.add_widget(_)
+        _ = GridLayout(cols=2)
+        _.add_widget(Label(text='Version (default: 1)'))
+        self.version.version = TextInput(multiline=False, text='1', size_hint=(None, None), width=32, height=32)
+        _.add_widget(self.version.version)
+        self.add_widget(_)
         
-        self.btn_get = Button(text='Get', on_press=main.get)
+        self.btn_get = Button(text='Get', on_press=main.get, size_hint_y=None, height=32)
         self.add_widget(self.btn_get)
-        self.result = GridLayout(cols=3)
-        self.result.result = TextInput(multiline=False, height=32)
+        self.result = GridLayout(cols=3, size_hint_y=None, height=40)
+        self.result.result = TextInput(multiline=False, size_hint=(None, None), width=220, height=32)
         self.result.add_widget(self.result.result)
-        self.result.show = CheckBox(size_hint=(None, None))
+        self.result.show = CheckBox(size_hint=(None, None), width=32, height=32)
         self.result.show.bind(active=self.check_show)
         self.result.add_widget(self.result.show)
-        self.result.add_widget(Label(text='Show', size_hint=(None, None)))
+        self.result.add_widget(Label(text='Show', size_hint=(None, None), width=32, height=32))
         self.add_widget(self.result)
         
     def check_show(self, checkbox, value):
@@ -145,7 +165,7 @@ class Menu(GridLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.cols = 2
-        self.exit = Button(text='<')
+        self.exit = Button(text='<', size_hint=(None, None), width=45, height=45, center=(0.5, 0.5))
         self.exit.bind(on_press=main.btn_exit)
         self.add_widget(self.exit)
 
